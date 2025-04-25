fe3c59be9bdbf41899508d7c3fc6fc2bdfc713d0
sakibrahat <sadmansakibrahat@gmail.com>
2025-04-26 02:27:25 +0600

    setting.py fix

--- registration/settings.py
+++ registration/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['el-petro.onrender.com']
 
 # Application definition
 
